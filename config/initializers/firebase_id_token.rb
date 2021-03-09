92a538e81d775071bb8b83a734472eb3a2e665d4
kko1ke <k.koike@daitecjp.com>
2021-03-09 13:37:36 +0900

    miscellaneous

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.project_ids = ['tich-de99a']
+  config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
   config.redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
 end
