c9722b570c20f6c16852c2e2c59381ac0f8bbf38
keke <dondohare55@gmail.com>
2021-03-24 21:06:03 +0900

    add search func for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < ApplicationController
   def index
-    @news = News.order(original_created_at: 'DESC').page params[:page]
+    @news = News.search(params[:symbol]).page params[:page]
   end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,9 @@
 class News < ApplicationRecord
   paginates_per 15
+
+  def self.search(symbol)
+    news = News.order(original_created_at: 'DESC')
+    news = news.where(symbol: symbol) if symbol.present?
+    news
+  end
 end
