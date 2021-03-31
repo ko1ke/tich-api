97f2a85734420ae609703d92d383bbfc239c97d5
keke <dondohare55@gmail.com>
2021-03-31 19:41:11 +0900

    add colmn for sti

---
+++ db/migrate/20210331012906_add_type_to_news.rb
@@ -0,0 +1,5 @@
+class AddTypeToNews < ActiveRecord::Migration[6.0]
+  def change
+    add_column :news, :type, :string, null:false, default: 'News::Company'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_023326) do
+ActiveRecord::Schema.define(version: 2021_03_31_012906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_023326) do
     t.string "symbol"
     t.datetime "original_created_at"
     t.string "original_id"
+    t.string "type", default: "News::Company", null: false
   end
 
   create_table "portfolios", force: :cascade do |t|
