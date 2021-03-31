07c6ce9b11a8af7179a72c4bb2d38cf31a10ff64
keke <dondohare55@gmail.com>
2021-03-31 19:41:53 +0900

    add market news fetch

--- app/jobs/finnhub_fetch_job.rb
+++ app/jobs/finnhub_fetch_job.rb
@@ -1,8 +1,17 @@
 class FinnhubFetchJob < ApplicationJob
   queue_as :default
 
-  def perform(limit_num: 3, symbols: Ticker.all.pluck(:symbol))
-    # Ex. URL: https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-03-01&to=2021-03-09&token=[TOKEN]
+  def perform(limit_num: 3, symbols: Ticker.all.pluck(:symbol), klass: News::Company)
+    unless [News::Company, News::Market].include?(klass)
+      raise ArgumentError,
+            'klass must be News::Company or News::Market'
+    end
+
+    # Market news doesn't need symbol
+    symbols = [nil] if klass == News::Market
+
+    # Ex for company. URL: https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-03-01&to=2021-03-09&token=[TOKEN]
+    # Ex for market. URL: https://finnhub.io/api/v1/news?category=general&token=[TOKEN]
 
     connection = Faraday.new(url: 'https://finnhub.io')
     today = Time.new.strftime('%Y-%m-%d')
@@ -11,7 +20,12 @@ class FinnhubFetchJob < ApplicationJob
     symbols.each do |symbol|
       timer = Timer.new(1)
 
-      response = connection.get "/api/v1/company-news?symbol=#{symbol}&from=#{one_week_ago}&to=#{today}&token=#{ENV['FINHUB_TOKEN']}"
+      if klass == News::Company
+        response = connection.get "/api/v1/company-news?symbol=#{symbol}&from=#{one_week_ago}&to=#{today}&token=#{ENV['FINHUB_TOKEN']}"
+      elsif klass == News::Market
+        response = connection.get "/api/v1/news?token=#{ENV['FINHUB_TOKEN']}"
+      end
+
       res_boby = JSON.parse(response.body)
 
       res_boby[0..limit_num - 1].each do |news|
@@ -19,16 +33,16 @@ class FinnhubFetchJob < ApplicationJob
         body = news['summary']
         link_url = news['url']
         image_url = news['image']
-        fetched_from = 'Finnhub'
+        fetched_from = "#{news['source']} via Finnhub"
         original_created_at = Time.at(news['datetime'])
         original_id = news['id']
 
-        News.find_or_create_by!(headline: headline, body: body,
-                                link_url: link_url,
-                                image_url: image_url,
-                                fetched_from: fetched_from, symbol: symbol,
-                                original_created_at: original_created_at,
-                                original_id: original_id)
+        klass.find_or_create_by!(headline: headline, body: body,
+                                 link_url: link_url,
+                                 image_url: image_url,
+                                 fetched_from: fetched_from, symbol: symbol,
+                                 original_created_at: original_created_at,
+                                 original_id: original_id)
         timer.set_time_remaining
         sleep(timer.time_remaining)
       end
