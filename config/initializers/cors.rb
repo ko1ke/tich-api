bdb8082142339e52bf654b61a3642eef199ddfbc
keke <dondohare55@gmail.com>
2021-05-06 11:15:05 +0900

    modify cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://tich-de99a.web.app'
 
     resource '*',
              headers: :any,
