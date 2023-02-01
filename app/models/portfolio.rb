e0094c134b59fb5c7bbeefe4a8fe80e5ba0c0e7e
keke <dondohare55@gmail.com>
2023-02-01 12:02:03 +0900

    remove unused methods

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,9 +3,4 @@ class Portfolio < ApplicationRecord
 
   PROFILE_JSON_SCHEMA = Rails.root.join('config', 'schemas', 'portfolio_schema.json').to_s
   validates :sheet, json: { schema: PROFILE_JSON_SCHEMA }
-
-  def self.tiker_symbols
-    all_sheet = Portfolio.pluck(:symbol).flatten
-    all_sheet.map { |sheet| sheet['ticker'] }.uniq
-  end
 end
