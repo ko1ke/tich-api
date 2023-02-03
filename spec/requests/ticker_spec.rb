a66a77a6d4d4851cd7fca6b8b21646594922b1e5
keke <dondohare55@gmail.com>
2023-02-03 13:51:42 +0900

    ticker request spec

---
+++ spec/requests/ticker_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Ticker', type: :request do
+  let(:ticker) { create(:ticker) }
+
+  describe '#index' do
+    it 'return status 200' do
+      get '/tickers.json'
+      expect(response).to have_http_status(200)
+    end
+    it 'return tickers' do
+      ticker
+      get '/tickers.json'
+      expect(JSON.parse(response.body).first['id']).to eq ticker.id
+    end
+  end
+end
