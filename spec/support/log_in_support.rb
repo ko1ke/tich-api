1d0c4bdca02a6a171d094a6ce3d9956772ec7800
keke <dondohare55@gmail.com>
2023-02-04 12:56:02 +0900

    login stub

---
+++ spec/support/log_in_support.rb
@@ -0,0 +1,12 @@
+module LogInSupport
+  RSpec.shared_context 'login_support' do
+    let(:current_user) { create(:user, :login_user) }
+    before do
+      allow_any_instance_of(Firebase::Auth::Authenticable).to receive(:authenticate_entity).and_return(current_user)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include LogInSupport, type: :request
+end
