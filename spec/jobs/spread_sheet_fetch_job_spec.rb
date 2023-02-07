1c60c6ff0ce7a1ba2c8e38a6106c69ad73b39abf
keke <dondohare55@gmail.com>
2023-02-07 10:37:12 +0900

    job spec fof SpreadSheetFetchJob

---
+++ spec/jobs/spread_sheet_fetch_job_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'SpreadSheetFetchJob', type: :job do
+  let(:exec_path) { 'this_is_dummy' }
+  let(:response_body) { [{ "symbol": 'MSFT', "formalName": 'Microsoft', "price": 200, 'change': 2 }].to_json }
+
+  before do
+    ActiveJob::Base.queue_adapter = :test
+
+    WebMock.enable!
+    ENV['GAS_EXEC_PATH'] = exec_path
+    WebMock.stub_request(:get, "https://script.google.com/#{exec_path}")
+           .to_return(
+             body: response_body,
+             status: 200,
+             headers: { 'Content-Type' => 'application/json' }
+           )
+  end
+  after do
+    WebMock.enable!
+  end
+
+  describe '#perform_now' do
+    let(:ticker) { create(:ticker, symbol: 'MSFT', formal_name: 'Microsoft', price: 1) }
+
+    context 'ticker symbol is not registered yet' do
+      it 'create tickers' do
+        expect do
+          SpreadSheetFetchJob.perform_now
+        end.to change(Ticker.all, :count).by(1)
+      end
+    end
+    context 'ticker symbol has been registered already' do
+      it 'update tickers' do
+        t = ticker
+        aggregate_failures do
+          expect do
+            SpreadSheetFetchJob.perform_now
+          end.to change(Ticker.all, :count).by(0)
+          expect(Ticker.first.symbol).to eq(t.symbol)
+          expect(Ticker.first.price).to eq(200)
+          expect(Ticker.first.change).to eq(2)
+        end
+      end
+    end
+  end
+
+  describe '#perform_later' do
+    it 'enqueue job' do
+      expect do
+        SpreadSheetFetchJob.perform_later
+      end.to have_enqueued_job
+    end
+  end
+end
