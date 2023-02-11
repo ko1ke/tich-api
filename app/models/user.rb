77eff26bdf55461996f0a215c5ac5fef0ea0ae4a
keke <dondohare55@gmail.com>
2023-02-11 21:49:22 +0900

    instance methods for rank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_newses, through: :favorites, source: :news
   has_many :newses
-
   validates :uid, presence: true, uniqueness: true
+
+  enum rank: { bronze: 0, silver: 1, gold: 2 } do
+    event :rank_up do
+      transition bronze: :silver
+      transition silver: :gold
+    end
+  end
 
   def registered_symbols
     return nil if portfolio&.sheet.blank?
